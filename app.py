ec5423deab66f9388b5c0ec2c21165c05519a386
idukkigold <nithish.nitt@gmail.com>
2018-12-08 21:56:07 -0500

    updated

--- app.py
+++ app.py
@@ -11,67 +11,44 @@ def index():
 @app.route('/get-user-data', methods=['POST'])
 def predict_stuff():
     if request.method == 'POST':
-        model = joblib.load('trained_house_classifier_model.pkl')
+        model = joblib.load('trained_anomaly detection.pkl')
 
-        print('-----line 27--------')
-        print(request.form.get('year_built'))
+        age = int(request.form.get('age'))
+        sex = int(request.form.get('sex'))
+        height = int(request.form.get('height'))
+        weight = int(request.form.get('weight'))
+        QRS_duration = int(request.form.get('QRS_duration'))
+        PR = int(request.form.get('PR'))
+        QT = int(request.form.get('QT'))
+        T = int(request.form.get('T'))
+        P = request.form.get('P')
+        aQRST = request.form.get('aQRST')
+        J = request.form.get('J')
 
-        year_built = int(request.form.get('year_built'))
+        heart_rate = request.form.get('heart_rate')
 
-        print('line 31')
+        wQ = request.form.get('wQ')
 
-        stories = int(request.form.get('stories'))
-        num_bedrooms = int(request.form.get('num_bedrooms'))
-        full_bathrooms = int(request.form.get('full_bathrooms'))
-        half_bathrooms = int(request.form.get('half_bathrooms'))
-        livable_sqft = int(request.form.get('livable_sqft'))
-        total_sqft = int(request.form.get('total_sqft'))
-        garage_sqft = int(request.form.get('garage_sqft'))
+        wR = request.form.get('wR')
 
-        carport_sqft = int(request.form.get('carport_sqft'))
-        has_fireplace = request.form.get('has_fireplace')
+        wS = request.form.get('wS')
+        wR_ = request.form.get('wR_')
 
-        has_pool = request.form.get('has_pool')
-
-        has_central_heating = request.form.get('has_central_heating')
-
-        has_central_cooling = request.form.get('has_central_cooling')
+        webdata = [age,sex,height,weight,QRS_duration,PR,QT,T,P,aQRST,J, heart_rate,wQ,wR, wS ,wR_,1,1,1,1,1,1]
 
 
-        has_fireplace = request.form.get('has_fireplace')
-
-        garage_type = request.form.get('garage_type')
- 
-        city = request.form.get('city')
-
-        house_to_value = [
-            # House features
-            year_built,   # year_built
-            stories,      # stories
-            num_bedrooms,      # num_bedrooms
-            full_bathrooms,      # full_bathrooms
-            half_bathrooms,      # half_bathrooms 
-            livable_sqft,   # livable_sqft
-            total_sqft,   # total_sqft
-            garage_sqft,      # garage_sqft
-            carport_sqft,      # carport_sqft
-
-        ]
-
-        # scikit-learn assumes you want to predict the values for lots of houses at once, so it expects an array.
-        # We just want to look at a single house, so it will be the only item in our array.
-        homes_to_value = [
-            house_to_value
+        webdata = [
+            webdata
         ]
 
         # return render_template("index.html", pred=house_to_value) 
 
         # Run the model and make a prediction for each house in the homes_to_value array
-        predicted_home_values = model.predict(homes_to_value)
+        predicted_value = model.predict(webdata)
 
         # Since we are only predicting the price of one house, just look at the first prediction returned
-        predicted_value = predicted_home_values[0]
-
+        predicted_value = predicted_value[0]
+        print(predicted_value)
         return render_template("index.html", pred=predicted_value) 
 
 if __name__ == "__main__":
